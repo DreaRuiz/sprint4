bbd5a394cdf500669ea1b3019c60a4bccef92cf4
Andrea Ruiz <info.drearuiz@gmail.com>
2022-11-10 11:57:28 +0100

    Exercise4

--- src/films.js
+++ src/films.js
@@ -3,7 +3,7 @@
 // Exercise 1: Get the array of all directors.
 function getAllDirectors(array) {
 	let result = array.map((movie) => movie.director);
-	/* 	console.log('EXERCICE 1 ->', result); */
+	console.log('EXERCICE 1 ->', result);
 	return result;
 }
 
@@ -15,14 +15,25 @@ function getMoviesFromDirector(array, director) {
 
 // Exercise 3: Calculate the average of the films of a given director.
 function moviesAverageOfDirector(array, director) {
-	let moviesFromDirector = getMoviesFromDirector(array, director); // Crea un array nou (moviesFromDirector) amb les pelis del director.
-	let scoreMoviesDirector = moviesFromDirector.reduce((acc, item) => acc + item.score, 0); // Suma les notes de totes les pelis de l'array nou començat per la posició 1 (0).
-	let moviesAverageDirector = Number((scoreMoviesDirector / moviesFromDirector.length).toFixed(2)); // Converteix en número, divideix la nota entre les pelis (per fer la mitja) i fes que el núero tingui 2 decimals.
+	let moviesFromDirector = getMoviesFromDirector(array, director); 	// Crea un array nou (moviesFromDirector) amb les pelis del director.
+	let scoreMoviesDirector = moviesFromDirector.reduce((acc, item) => acc + item.score, 0); 	// Suma les notes de totes les pelis de l'array nou començat per la posició 1 (0).
+	let moviesAverageDirector = Number((scoreMoviesDirector / moviesFromDirector.length).toFixed(2)); 	// Converteix en número, divideix la nota entre les pelis (per fer la mitja) i fes que el núero tingui 2 decimals.
 	return moviesAverageDirector;
 }
 
 // Exercise 4:  Alphabetic order by title
-function orderAlphabetically(array) {}
+
+function orderAlphabetically(array) {
+	let movieTitleList = array.map((movie) => movie.title); 	// Fa un array amb els títols de les pelis.
+	movieTitleList.sort();		// Endreça alfabeticament els elements de l'array.
+
+	if (movieTitleList.length > 20) {		// Si la mida de l'array és major a 20.
+		let top20movies = movieTitleList.slice(0, 20);		// Recorre l'array i afegeix de la posició 0 a la 20 al nou array (top20movies).
+		return top20movies;
+	} else {
+		return movieTitleList; 		// Si és té menys de 20 elements, retorna la llista base (ja endreçada).
+	}
+}
 
 // Exercise 5: Order by year, ascending
 function orderByYear() {}
